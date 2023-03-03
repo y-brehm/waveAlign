ed911ac4e650f1f9f093fd49eab82786e547cb4b
y-brehm <yannikbrehm@web.de>
2023-03-03 08:36:16 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from glob import glob
+from os.path import splitext, basename
+from setuptools import setup, find_packages
+
+
+setup(
+    name='waveAlign',
+    version=1.0,
+    description="A python package for loudness alignment of audio files (currently .wav only).",
+    url='https://github.com/y-brehm/waveAlign',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    python_requires='==3.9.*',
+    install_requires=[
+        'pyloudnorm==0.1.1',
+        'numpy==1.22.0',
+        'librosa==0.10.0',
+        'pyloudnorm==0.1.1',
+        'natsort~=8.2.0',
+        ],
+)
