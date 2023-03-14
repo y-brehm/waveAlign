cf31e8960b2e00bd362a13227a6dad2f2802375e
Simon Zimmermann <simon@dear-reality.com>
2023-03-14 15:02:14 +0100

    Add aiff support (#1)
    Adds aiff-support and album-art retention.
    
    Additionally:
    * Consolidate read and write to soundfile library and remove librosa
    * Remove unused field from AudioFileSpecSet

--- src/wavealign/data_collection/audio_file_spec_set.py
+++ src/wavealign/data_collection/audio_file_spec_set.py
@@ -3,9 +3,9 @@ from numpy import ndarray
 
 
 @dataclass
-class AudioSpecSet:
+class AudioFileSpecSet:
     file_path: str
     audio_data: ndarray
     original_lufs: float
     sample_rate: int
-    original_file_extension: str
+    artwork: bytearray
--- src/wavealign/wave_alignment.py
+++ src/wavealign/wave_alignment.py
@@ -1,9 +1,7 @@
 import os
 
-from soundfile import write
-
-from wavealign.data_collection.find_audio_files import find_audio_files
-from wavealign.data_collection.generate_audio_spec_set import generate_audio_spec_set
+from wavealign.data_collection.audio_file_finder import AudioFileFinder
+from wavealign.data_collection.audio_file_handler import read, write
 from wavealign.loudness_processing.align_waveform_to_target import align_waveform_to_target
 from wavealign.loudness_processing.calculation import detect_peak
 
@@ -17,26 +15,23 @@ def wave_alignment(
         ) -> None:
     try:
         lufs_values = []
-        for file_path in find_audio_files(os.path.normpath(input_path), file_ending='.wav'):
-            audio_spec_set = generate_audio_spec_set(file_path)
-            lufs_values.append(audio_spec_set.original_lufs)
-            print(f"Processing file: {file_path}, original LUFS: {audio_spec_set.original_lufs}")
+        for file_path in AudioFileFinder().find(os.path.normpath(input_path)):
+            audio_file_spec_set = read(file_path)
+            lufs_values.append(audio_file_spec_set.original_lufs)
+            print(f"Processing file: {file_path}, original LUFS: {audio_file_spec_set.original_lufs}")
             if read_only is False:
-                align_waveform_to_target(audio_spec_set, target_lufs)
+                align_waveform_to_target(audio_file_spec_set, target_lufs)
                 if check_for_clipping:
-                    peak_after_processing = detect_peak(audio_spec_set.audio_data)
+                    peak_after_processing = detect_peak(audio_file_spec_set.audio_data)
                     print(f"new PEAK value after processing: {peak_after_processing}")
 
                     assert (peak_after_processing <= 0)
 
                 if output_path is None:
-                    output = audio_spec_set.file_path
+                    output = audio_file_spec_set.file_path
                 else:
-                    output = os.path.join(output_path, os.path.split(audio_spec_set.file_path)[1])
-                write(output,
-                      audio_spec_set.audio_data,
-                      audio_spec_set.sample_rate,
-                      subtype='PCM_16')
+                    output = os.path.join(output_path, os.path.split(audio_file_spec_set.file_path)[1])
+                write(output, audio_file_spec_set)
         print(f"Total number of processed files: {len(lufs_values)}")
         print(f"Minimum overall LUFS-value: {min(lufs_values)} dB LUFS")
         print(f"Maximum overall LUFS-value: {max(lufs_values)} dB LUFS")
