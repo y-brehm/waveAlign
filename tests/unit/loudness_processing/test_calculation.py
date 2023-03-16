ee5c1d86823f9a3ad2d70daea00f05b456e4fe2e
y-brehm <yannikbrehm@web.de>
2023-03-16 19:09:07 +0100

    apply requested review changes

--- tests/unit/loudness_processing/test_calculation.py
+++ tests/unit/loudness_processing/test_calculation.py
@@ -1,8 +1,12 @@
 import mock
 import unittest
+import math
+import numpy as np
+
+from parameterized import parameterized
 
 from wavealign.loudness_processing.calculation import \
-    calculate_gain_difference_to_target, detect_peak, calculate_lufs, gain_to_db
+    calculate_gain_difference_to_target, detect_peak, calculate_lufs, gain_to_db, db_to_gain
 
 
 class TestCalculation(unittest.TestCase):
@@ -22,19 +26,29 @@ class TestCalculation(unittest.TestCase):
 
         mock_gain_to_db.assert_called_once_with(5)
 
+    @parameterized.expand([
+        ("louder target than input", -12, -10, 1.258925, 2),
+        ("louder input than target", -10, -12, 0.794328, -2),
+        ("no loudness difference", -12, -12, 0, 0),
+        ("zero case", 0, 0, 0, 0)
+        ])
     @mock.patch('wavealign.loudness_processing.calculation.db_to_gain')
-    def test_calculate_gain_difference_to_target(self, mock_db_to_gain):
-        fake_input_lufs = -12
-        fake_lufs_target = -10
-        fake_output_gain = 1.258925
-
+    def test_calculate_gain_difference_to_target(
+            self,
+            test_case,
+            fake_input_lufs,
+            fake_lufs_target,
+            fake_output_gain,
+            fake_output_level,
+            mock_db_to_gain
+            ):
         mock_db_to_gain.return_value = fake_output_gain
 
         fake_output = calculate_gain_difference_to_target(
             fake_input_lufs, fake_lufs_target
         )
 
-        mock_db_to_gain.assert_called_once_with(2)
+        mock_db_to_gain.assert_called_once_with(fake_output_level)
         self.assertEqual(fake_output, fake_output_gain)
 
     @mock.patch('wavealign.loudness_processing.calculation.Meter')
@@ -53,3 +67,23 @@ class TestCalculation(unittest.TestCase):
         fake_gain = -5
         gain_to_db(fake_gain)
         mock_np_log.assert_called_once_with(fake_gain)
+
+    @parameterized.expand([
+        ("base case", 5, 13.979400086720377),
+        ("low case", 1, 0),
+        ("high case", 30, 29.54242509439325),
+        ("zero case", 0, -math.inf),
+        ])
+    def test_gain_to_db_values(self, test_case, fake_gain, expected):
+        fake_db = gain_to_db(fake_gain)
+        np.testing.assert_almost_equal(fake_db, expected)
+
+    @parameterized.expand([
+        ("base case", 5, 1.7782794100389228),
+        ("low case", 1, 1.1220184543019633),
+        ("high case", 30, 31.622776601683793),
+        ("zero case", 0, 1),
+    ])
+    def test_db_to_gain_values(self, test_case, fake_db, expected):
+        fake_gain = db_to_gain(fake_db)
+        np.testing.assert_almost_equal(fake_gain, expected)
