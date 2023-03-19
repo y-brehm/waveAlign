03337c55a6677761fab6f24f1634721e9ac7b7d2
Maxim <111158251+maosi100@users.noreply.github.com>
2023-03-19 18:27:25 +0100

    Issue/#2/add unit tests data collection (#8)
    Issue/#2/add unit tests data collection

---
+++ tests/unit/data_collection/test_audio_file_finder.py
@@ -0,0 +1,47 @@
+import mock
+import unittest
+
+from wavealign.data_collection.audio_file_finder import AudioFileFinder
+
+class TestAudioFileFinder(unittest.TestCase):
+
+    def setUp(self):
+        self.audio_file_finder = AudioFileFinder()
+        self.fake_start_dir = '/my/fake/dir'
+        self.fake_file_dir = [
+                ('/my/fake/dir', ['subdir'], ['fake_file_1.wav', 'fake_file_2.txt']),
+                ('/my/fake/dir/subdir', [], ['fake_file_3.aiff'])
+                ]
+        self.expected_output = [
+                '/my/fake/dir/fake_file_1.wav',
+                '/my/fake/dir/subdir/fake_file_3.aiff'
+                ]
+    
+    @mock.patch('wavealign.data_collection.audio_file_finder.os.path.abspath')
+    @mock.patch('wavealign.data_collection.audio_file_finder.os.walk')
+    @mock.patch('wavealign.data_collection.audio_file_finder.AudioFileFinder._AudioFileFinder__is_supported_audio_file')
+    def test_find_calls(self, mock_AudioFileFinder__is_supported_audio_file, mock_walk, mock_abspath):
+        mock_abspath.return_value = self.fake_start_dir
+        mock_walk.return_value = self.fake_file_dir
+        mock_AudioFileFinder__is_supported_audio_file.side_effect = [True, False, True]
+
+        fake_output = self.audio_file_finder.find(self.fake_start_dir)
+
+        mock_abspath.assert_called_once_with(self.fake_start_dir)
+        mock_walk.assert_called_once_with(self.fake_start_dir)
+        mock_AudioFileFinder__is_supported_audio_file.assert_has_calls([
+            mock.call('fake_file_1.wav'),
+            mock.call('fake_file_2.txt'),
+            mock.call('fake_file_3.aiff')
+            ])
+
+        self.assertEqual(fake_output, self.expected_output)
+
+    @mock.patch('wavealign.data_collection.audio_file_finder.os.walk')
+    def test_find_functionality(self, mock_walk):
+        mock_walk.return_value = self.fake_file_dir
+
+        fake_output = self.audio_file_finder.find(self.fake_start_dir)
+
+        mock_walk.assert_called_once_with(self.fake_start_dir)
+        self.assertEqual(fake_output, self.expected_output)
---
+++ tests/unit/data_collection/test_audio_file_handler.py
@@ -0,0 +1,64 @@
+import mock
+import unittest
+from wavealign.data_collection.audio_file_spec_set import AudioFileSpecSet
+
+from wavealign.data_collection.audio_file_handler import read, write
+
+class TestAudioFileHandler(unittest.TestCase):
+    def setUp(self):
+        self.fake_file_path = '/my/fake/dir/fake_file_1.wav'
+        self.fake_artwork = bytearray([1, 2, 3, 4, 5])
+        self.fake_audio_data = mock.MagicMock()
+        self.fake_sample_rate = 44100
+        self.fake_original_lufs = 7.0
+
+    @mock.patch('wavealign.data_collection.audio_file_handler.load_file')
+    @mock.patch('wavealign.data_collection.audio_file_handler.soundfile.read')
+    @mock.patch('wavealign.data_collection.audio_file_handler.calculate_lufs')
+    def test_read(self, mock_calculate_lufs, mock_read, mock_load_file):
+        
+        mock_load_file.return_value = {
+                'no artwork': "test",
+                'artwork': self.fake_artwork  
+                }
+        mock_read.return_value = (self.fake_audio_data, self.fake_sample_rate)
+        mock_calculate_lufs.return_value = self.fake_original_lufs
+       
+        fake_output = read(self.fake_file_path)
+        
+        self.assertIsInstance(fake_output, AudioFileSpecSet)
+        self.assertEqual(fake_output.file_path, self.fake_file_path)
+        self.assertEqual(fake_output.audio_data, self.fake_audio_data)
+        self.assertEqual(fake_output.sample_rate, self.fake_sample_rate)
+        self.assertEqual(fake_output.artwork, self.fake_artwork)
+        self.assertEqual(fake_output.original_lufs, self.fake_original_lufs)
+
+        mock_load_file.assert_called_once_with(self.fake_file_path)
+        mock_read.assert_called_once_with(self.fake_file_path)
+        mock_calculate_lufs.assert_called_once_with(*mock_read.return_value)
+   
+
+    @mock.patch('wavealign.data_collection.audio_file_handler.soundfile.write')
+    @mock.patch('wavealign.data_collection.audio_file_handler.load_file')
+    def test_write(self, mock_load_file, mock_soundfile_write):
+        fake_audio_file_spec_set = AudioFileSpecSet(
+                file_path=self.fake_file_path,
+                audio_data=self.fake_audio_data,
+                original_lufs=self.fake_original_lufs,
+                sample_rate=self.fake_sample_rate,
+                artwork=self.fake_artwork,
+                )
+
+        write(self.fake_file_path, fake_audio_file_spec_set)
+
+        mock_soundfile_write.assert_called_once_with(
+                self.fake_file_path,
+                self.fake_audio_data,
+                self.fake_sample_rate,
+                subtype='PCM_16'
+                )
+
+        mock_load_file.assert_called_once_with(self.fake_file_path)
+        mock_load_file.return_value.__setitem__\
+                .assert_called_once_with('artwork', fake_audio_file_spec_set.artwork)
+        mock_load_file.return_value.save.assert_called_once()
