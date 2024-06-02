0ffc78277bb5d8390b4c34a5dea1ee45efe91663
Maxim Osipovs <owsipovs@hotmail.de>
2024-06-02 20:44:19 +0200

    fixed string to non f-string

--- tests/unit/data_collection/test_logging_configuration.py
+++ tests/unit/data_collection/test_logging_configuration.py
@@ -2,7 +2,9 @@ import unittest
 import mock
 
 from wavealign.data_collection.logging_configuration import (
-    create_logging_config, setup_logging, output_logfile_warning
+    create_logging_config,
+    setup_logging,
+    output_logfile_warning,
 )
 
 
@@ -13,35 +15,29 @@ class TestLoggingConfiguration(unittest.TestCase):
 
         create_logging_config(output_path)
 
-        mock_os_path_join.assert_called_once_with(
-            output_path, "wavealign.log"
-        )
-    
+        mock_os_path_join.assert_called_once_with(output_path, "wavealign.log")
+
     @mock.patch("logging.config.dictConfig")
     @mock.patch("wavealign.data_collection.logging_configuration.create_logging_config")
-    def test_setup_logging(
-            self, 
-            mock_create_logging_config,
-            mock_config_dictConfig
-    ):
+    def test_setup_logging(self, mock_create_logging_config, mock_config_dictConfig):
         output_path = "/path/to/directory"
         setup_logging(output_path)
         mock_create_logging_config.assert_called_once_with(output_path)
-        mock_config_dictConfig.assert_called_once_with(mock_create_logging_config.return_value)
-    
+        mock_config_dictConfig.assert_called_once_with(
+            mock_create_logging_config.return_value
+        )
+
     @mock.patch("os.path.join", return_value="dummy_path")
     @mock.patch("os.path.exists", return_value=True)
     def test_output_logfile_warning(self, mock_os_path_exists, mock_os_path_join):
         output_path = "/path/to/directory"
-        
+
         with mock.patch("builtins.print") as mock_print:
             output_logfile_warning(output_path)
 
-        mock_os_path_join.assert_called_once_with(
-            output_path, "wavealign.log"
-        )
+        mock_os_path_join.assert_called_once_with(output_path, "wavealign.log")
         mock_os_path_exists.assert_called_once_with("dummy_path")
         mock_print.assert_called_once_with(
-            f"\nSome files were not processed successfully. "
-            f"A log file at dummy_path was created."
+            "\nSome files were not processed successfully. "
+            "A log file at dummy_path was written."
         )
