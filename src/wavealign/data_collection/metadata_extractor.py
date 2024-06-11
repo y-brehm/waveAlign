ac1bb5fb1e44bb9456d4a87cac2cb8083b5481db
Maxim Osipovs <owsipovs@hotmail.de>
2024-06-11 21:31:28 +0200

    removed metadata check

--- src/wavealign/data_collection/metadata_extractor.py
+++ src/wavealign/data_collection/metadata_extractor.py
@@ -9,24 +9,18 @@ class MetaDataExtractor:
         self,
         file_path: str,
     ) -> AudioMetadata:
-        try:
-            metadata = File(file_path)
-            if metadata is None:
-                raise ValueError
+        metadata = File(file_path)
+        full_details = probe.full_details(file_path)
+        audio_stream_metadata = full_details["streams"][0]
+        bit_rate = self.__get_bitrate_specifier(audio_stream_metadata["bit_rate"])
 
-            full_details = probe.full_details(file_path)
-            audio_stream_metadata = full_details["streams"][0]
-            bit_rate = self.__get_bitrate_specifier(audio_stream_metadata["bit_rate"])
-
-            return AudioMetadata(
-                num_channels=audio_stream_metadata["channels"],
-                metadata=metadata,
-                codec_name=audio_stream_metadata["codec_name"],
-                bit_rate=bit_rate,
-                sample_rate=audio_stream_metadata["sample_rate"],
-            )
-        except ValueError:
-            raise Exception(f"Failed to read metadata for {file_path}")
+        return AudioMetadata(
+            num_channels=audio_stream_metadata["channels"],
+            metadata=metadata,
+            codec_name=audio_stream_metadata["codec_name"],
+            bit_rate=bit_rate,
+            sample_rate=audio_stream_metadata["sample_rate"],
+        )
 
     @staticmethod
     def __get_bitrate_specifier(bit_rate: int) -> str:
