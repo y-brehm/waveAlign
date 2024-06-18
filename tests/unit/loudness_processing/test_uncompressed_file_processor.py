b568e08d5bb93e3b965bb1b6fe6bc5691cea466b
Simon Zimmermann <mail@simonzimmermann.de>
2024-06-18 22:02:21 +0200

    add uncompressed_file test

---
+++ tests/unit/loudness_processing/test_uncompressed_file_processor.py
@@ -0,0 +1,33 @@
+import mock
+import unittest
+import numpy as np
+
+from wavealign.loudness_processing.uncompressed_file_processor import (
+    UncompressedFileProcessor,
+)
+
+
+class TestUncompressedFileProcessor(unittest.TestCase):
+    @mock.patch(
+        "wavealign.loudness_processing.uncompressed_file_processor.align_waveform_to_target"
+    )
+    @mock.patch(
+        "wavealign.loudness_processing.uncompressed_file_processor.AudioFileWriter"
+    )
+    def test_process(self, mock_audio_file_writer, mock_align_waveform):
+        audio_property_set = mock.MagicMock()
+        target_level = -10
+        output_file_path = "output.wav"
+        audio_data = mock.MagicMock()
+
+        UncompressedFileProcessor().process(
+            audio_property_set, target_level, output_file_path, audio_data
+        )
+        mock_audio_file_writer.return_value.write.assert_called_once_with(
+            output_file_path,
+            mock_align_waveform.return_value,
+            audio_property_set.metadata,
+        )
+        mock_align_waveform.assert_called_once_with(
+            audio_data, audio_property_set.original_lufs_level, target_level
+        )
