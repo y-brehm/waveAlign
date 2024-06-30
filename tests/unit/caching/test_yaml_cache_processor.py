f3e3c1708923c02bdfc4553ac25471053b961d57
y-brehm <yannikbrehm@web.de>
2024-06-30 14:35:32 +0200

    adapt new test structure to yaml cache processor

--- tests/unit/caching/test_yaml_cache_processor.py
+++ tests/unit/caching/test_yaml_cache_processor.py
@@ -7,46 +7,53 @@ from wavealign.caching.yaml_cache_processor import YamlCacheProcessor
 
 
 class TestYamlCacheProcessor(unittest.TestCase):
-    @mock.patch("wavealign.caching.yaml_cache_processor.os.path.exists")
-    def test_read_cache_non_existent(self, mock_exists):
-        mock_exists.return_value = False
+    def setUp(self):
+        self.mock_open = mock.patch(
+            "builtins.open", new_callable=mock_open
+        ).start()
+        self.mock_os_path_exists = mock.patch("os.path.exists").start()
+
+    def tearDown(self):
+        mock.patch.stopall()
+
+    def test_read_cache_non_existent(self):
+        self.mock_os_path_exists.return_value = False
 
         result = YamlCacheProcessor("/fake/path").read_cache()
 
         self.assertEqual(result, None)
 
-    @mock.patch("builtins.open", new_callable=mock_open, read_data="key: value\n")
-    @mock.patch("os.path.exists")
     @mock.patch("wavealign.caching.yaml_cache_processor.yaml.safe_load")
     @mock.patch("wavealign.caching.yaml_cache_processor.DictToDataclassConverter")
     def test_read_cache_exists(
-        self, mock_dict_to_dataclass_converter, mock_yaml_load, mock_exists, mock_open
+        self, mock_dict_to_dataclass_converter, mock_yaml_load
     ):
-        mock_exists.return_value = True
+        self.mock_os_path_exists.return_value = True
         fake_yaml_dict = {"key": "value"}
         dummy_dataclass = mock.MagicMock()
 
         mock_yaml_load.return_value = fake_yaml_dict
-        mock_dict_to_dataclass_converter.return_value.process.return_value = dummy_dataclass
+        mock_dict_to_dataclass_converter.return_value.process.return_value = (
+            dummy_dataclass
+        )
 
         result = YamlCacheProcessor("/fake/path").read_cache()
 
-        mock_yaml_load.assert_called_once_with(mock_open())
+        mock_yaml_load.assert_called_once_with(self.mock_open())
         mock_dict_to_dataclass_converter.return_value.process.assert_called_once_with(
             YamlCache, fake_yaml_dict
         )
         self.assertEqual(result, dummy_dataclass)
 
-    @mock.patch("builtins.open", new_callable=mock_open)
     @mock.patch("wavealign.caching.yaml_cache_processor.yaml.dump")
-    def test_write_cache(self, mock_yaml_dump, mock_open):
+    def test_write_cache(self, mock_yaml_dump):
         cache_data = YamlCache(processed_files=[], target_level=0.5)
 
         cache_dict = {"processed_files": [], "target_level": 0.5}
 
         YamlCacheProcessor("/fake/path").write_cache(cache_data)
 
-        mock_open.assert_called_once_with("/fake/path/.wavealign_cache.yaml", "w")
+        self.mock_open.assert_called_once_with("/fake/path/.wavealign_cache.yaml", "w")
         mock_yaml_dump.assert_called_with(
-            cache_dict, mock_open(), default_flow_style=False
+            cache_dict, self.mock_open(), default_flow_style=False
         )
